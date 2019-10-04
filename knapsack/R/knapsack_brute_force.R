fca9de10ba3c7112a9a65ca4b2409bfea8e25ed1
Laura Julià Melis <laaurajulia@gmail.com>
2019-10-04 16:00:48 +0200

    Started the brute force algorithm.

--- knapsack/R/knapsack_brute_force.R
+++ knapsack/R/knapsack_brute_force.R
@@ -33,5 +33,30 @@ knapsack_objects <-
 
 # Function
 brute_force_knapsack <- function(x, W){
+  combn <- 1:(2^nrow(x))
+  bin <- matrix(NA, nrow=2^nrow(x), ncol=nrow(x))
+  for(i in combn){
+    bin[i,]<- as.integer(head(intToBits(combn[i]), nrow(x))) 
+  }
   
+  weight <- W
+  elements <- vector()
+  value <- 0
+  for(i in 1:nrow(bin)){
+    temp_weight <- sum(x[,1][as.logical(bin[i,])])
+   
+     if(temp_weight <= W){
+      temp_value <- sum(x[,2][as.logical(bin[i,])])
+      
+      if(temp_value > value || (temp_value == value && temp_weigth < weight)){
+        weigth <- temp_weight
+        value <- temp_value
+        elements <- which(as.logical(bin[i,]))
+      }
+    }
+  }
+  
+  return(list("value"=value, "elements"=elements))
 }
+
+
