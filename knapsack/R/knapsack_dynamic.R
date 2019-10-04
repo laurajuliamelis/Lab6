a0848ea60d9301af6806e8011b8a0d90ab20dd07
Laura Julià Melis <laaurajulia@gmail.com>
2019-10-04 14:29:51 +0200

    Created function files.

---
+++ knapsack/R/knapsack_dynamic.R
@@ -0,0 +1,37 @@
+#' GET_votation
+#'
+#' \code{GET_votation} makes a GET request for data from the 
+#' Swedish Parlament API, specifically for votations.
+#' 
+#' 
+#' @param period a 
+#' @param span b
+
+#' @return 
+#'
+#' @examples
+#' brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
+#' brute_force_knapsack(x = knapsack_objects[1:12,], W = 3500)
+#' brute_force_knapsack(x = knapsack_objects[1:8,], W = 2000)
+#' brute_force_knapsack(x = knapsack_objects[1:12,], W = 2000)
+#' 
+#' @references \url{http://data.riksdagen.se/}
+#'
+#' @importFrom xml2 read_xml
+#'
+#' @export
+#' 
+
+# Data
+set.seed(42)
+n <- 2000
+knapsack_objects <-
+  data.frame(
+    w=sample(1:4000, size = n, replace = TRUE),
+    v=runif(n = n, 0, 10000)
+  )
+
+# Function
+knapsack_dynamic <- function(x, W){
+  
+}
