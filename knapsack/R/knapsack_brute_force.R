4f420300e5ae5c47fa20a74063a287cc4cebfede
Martin <martin.k.r.svensson@gmail.com>
2019-10-06 00:44:04 +0200

    Tested parallel computing.

--- knapsack/R/knapsack_brute_force.R
+++ knapsack/R/knapsack_brute_force.R
@@ -22,20 +22,11 @@
 #' @export
 #' 
 
-# Data
-set.seed(42)
-n <- 2000
-knapsack_objects <-
-  data.frame(
-    w=sample(1:4000, size = n, replace = TRUE),
-    v=runif(n = n, 0, 10000)
-  )
-
 # Function
 brute_force_knapsack <- function(x, W){
   combn <- 1:(2^nrow(x))
   bin <- matrix(NA, nrow=2^nrow(x), ncol=nrow(x))
-  for(i in combn){
+  for(i in 1:(2^nrow(x))){
     bin[i,]<- as.integer(head(intToBits(combn[i]), nrow(x))) 
   }
   
@@ -48,7 +39,7 @@ brute_force_knapsack <- function(x, W){
      if(temp_weight <= W){
       temp_value <- sum(x[,2][as.logical(bin[i,])])
       
-      if(temp_value > value || (temp_value == value && temp_weigth < weight)){
+      if(temp_value > value || (temp_value == value && temp_weight < weight)){
         weight <- temp_weight
         value <- temp_value
         elements <- which(as.logical(bin[i,]))
