e993cb93346c1c6a0809f0bc42be75b70103d0b9
Martin <martin.k.r.svensson@gmail.com>
2019-10-11 10:11:45 +0200

    Fixed documentation and note.

--- knapsack/vignettes/vignette.R
+++ knapsack/vignettes/vignette.R
@@ -61,3 +61,23 @@ greedy_knapsack(x = knapsack_objects[1:8,], W = 3500)
 ## ---- eval=FALSE---------------------------------------------------------
 #  microbenchmark(brute_force_knapsack(x = knapsack_objects[1:20,], W = 3500, parallel = TRUE), times = 1)
 
+## ---- eval=FALSE---------------------------------------------------------
+#  microbenchmark(brute_force_knapsack(x = knapsack_objects[1:20,], W = 3500, parallel = TRUE, fast=TRUE), times = 1)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  microbenchmark(brute_force_knapsack(x = knapsack_objects[1:20,], W = 3500), times = 1)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  microbenchmark(brute_force_knapsack(x = knapsack_objects[1:20,], W = 3500, parallel = TRUE), times = 1)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  microbenchmark(knapsack_dynamic(x = knapsack_objects[1:500,], W = 3500), times = 5)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  set.seed(42)
+#  n <- 1000000
+#  knapsack_objects <- data.frame( w=sample(1:4000, size = n, replace = TRUE),
+#                                  v=runif(n = n, 0, 10000))
+#  
+#  microbenchmark(greedy_knapsack(x = knapsack_objects[1:1000000,], W = 3500), times = 5)
+
